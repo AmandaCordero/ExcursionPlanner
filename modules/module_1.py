8f96c309ac2e51fc704b30f410cbc6abe6f495b5
anuno61044 <anuno61044@gmail.com>
2024-09-10 07:13:35 -0400

    add doc string to script methods

--- modules/module_1.py
+++ modules/module_1.py
@@ -28,8 +28,25 @@ def plan_route(map_data, tourist_preferences):
     # Calcular la mejor ruta con A*
     return a_star_search(map_data, characteristics)
 
-
 def a_star_search(map, characteristics):
+    """
+    Implementa el algoritmo A* Search para encontrar la ruta que aporte más satisfacción según los intereses de los turistas.
+
+    Args:
+        map (object): Datos del mapa, incluyendo información sobre puntos de interés y caminos.
+        characteristics (list): Resumen de los intereses de los turistas. 
+
+    Returns:
+        (list): Lista con los puntos (Point) en el orden de la ruta escogida, None si no se puede encontrar
+
+    Note:
+        Este método utiliza una búsqueda A* para encontrar la ruta óptima considerando:
+        1. La estructura del grafo del mapa
+        2. El punto de inicio
+        3. Los intereses de los turistas
+        3. La función heurística utilizada para guiar la búsqueda
+        4. Las propiedades de los puntos y caminos en el mapa, además de la distancia
+    """
     
     # Obtener datos de interes del mapa
     start,exit = map.start, map.exit
@@ -99,8 +116,9 @@ def a_star_search(map, characteristics):
 
 
         for neighbor in get_neighbors(current, map):
+            
             # Se calcula el costo hasta este nuevo vecino
-            tentative_g_score = g_score[current] + heuristic(current, neighbor, map)
+            tentative_g_score = g_score[current] + map.paths_details[(current,neighbor)]['distance']
 
             if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
                 temp_came_from[neighbor] = current
@@ -111,49 +129,68 @@ def a_star_search(map, characteristics):
     return None
 
 def get_goal(start, map, interest_points_unvisited, default_goal, characteristics):
+    """
+    Determina el próximo objetivo de la excursión basándose en las preferencias de los turistas y características del mapa.
+
+    Args:
+        start (num): Id del punto actual
+        map (object): Datos del mapa, incluyendo información sobre puntos de interés y caminos.
+        interest_points_unvisited (list): Puntos de interés del mapa que aún no han sido visitados.
+        default_goal (num): Id del punto objetivo por defecto (generalmente la salida del mapa)
+        characteristics (list): Lista de preferencias resumidas de los turistas
+
+    Returns:
+        goal: Id del punto de interés seleccionado como objetivo.
+
+    Note:
+        Este método utiliza la función heurística para evaluar la atracción de cada punto de interés
+        basada en las preferencias del turista y las características del mapa.
+        El objetivo final es el punto de interés que maximiza la atracción según estas consideraciones.
+
+    Raises:
+        ValueError: Si no se puede encontrar un punto de interés adecuado basándose en las preferencias del turista
+    """
+    
     min_f,goal = heuristic(start, default_goal, map), default_goal
+    
     for point in interest_points_unvisited:
-        path_to_point_f = 0
         
-        path_to_point_f = heuristic(start, point, map) + heuristic(point, default_goal, map) - calculate_point_value(map, point, characteristics)
+        path_to_point_f = heuristic2(start, point, default_goal, characteristics, map)
         
         if path_to_point_f < min_f:
             goal = point
             min_f = path_to_point_f
-    print('goal: ', goal)
+            
     return goal
-
-# Valor estimado del punto según los intereses de los turistas
-def calculate_point_value(map, point_id, tourists_characteristics):
-    point = map.points[point_id]
-    value = 0
     
-    for i in range(6):
-        if point.characteristics[i] == 0 or tourists_characteristics[i] == 0:
-            continue
-        value += 1000*(1-abs(point.characteristics[i]-tourists_characteristics[i]))
-    
-    return value
-    
-# Costo estimado para ir de un punto a otro (sean adyacentes o no)
 def heuristic(start, end, map):
+    """
+    Calcula el costo aproximado entre dos puntos en el mapa.
+
+    Args:
+        start (num): Id del punto inicial del recorrido
+        end (num): Id del punto final del recorrido
+        map (object): Datos del mapa, incluyendo información sobre puntos de interés y caminos.
+
+    Returns:
+        cost: Costo estimado en el recorrido según la heurística utilizada
+
+    Note:
+        Esta función calcula la heurística utilizando la fórmula de Euclides para la distancia entre ambos puntos.
+        Además tiene en cuenta la pendiente del recorrido para calcular el costo.
+    """
+    
     if start == end:
         return 0
     
     point1 = map.points[start]
     point2 = map.points[end]
     
-    # Esta es la distancia que separa en el plano a los dos puntos sin tener en cuenta la altura
+    # Esta es la distancia entre los dos puntos sin tener en cuenta la altura
     distance = math.sqrt(abs(point1.location[0]-point2.location[0])**2 + (point1.location[1]-point2.location[1])**2)
     
-    # Calcular la distancia de subida que hay entre los puntos
-    elevation = 0
-    if point1.altitude == "hill_one" and (point2.altitude == 'hill_two' or point2.altitude == 'top'):
-        elevation = point2.height - 60
-    elif (point1.altitude == 'hill_two' or point1.altitude == 'top') and point2.altitude == "hill_one":
-        elevation = point2.height - 60
-    else:
-        elevation = point2.height - point1.height
+    # Diferencia de altura entre ambos puntos
+    elevation = point2.height - point1.height
     
     # Calculamos la distancia real entre los puntos teniendo en cuenta la altura
     real_distance = math.sqrt(distance**2 + elevation**2)
@@ -166,15 +203,70 @@ def heuristic(start, end, map):
     
     return cost
 
-# Obtener los puntos adyacentes al punto analizado
+def heuristic2(start, midpoint, end, characteristics, map):
+    """
+    Calcula el costo aproximado entre dos puntos en el mapa pasando por un punto intermedio.
+
+    Args:
+        start (num): Id del punto inicial del recorrido
+        midpoint (num): Id del punto intermedio del recorrido
+        end (num): Id del punto final del recorrido
+        map (object): Datos del mapa, incluyendo información sobre puntos de interés y caminos.
+
+    Returns:
+        Costo estimado en el recorrido según la heurística utilizada
+
+    Note:
+        Esta función utiliza la función heuristic con la peculiaridad de que se tiene en cuenta
+        los "beneficios" de pasar por el punto midpoint según los intereses de los turistas.
+
+    """
+    
+    # Calcular el valor de este punto
+    point = map.points[midpoint]
+    value = 0
+    for i in range(6):
+        if point.characteristics[i] == 0 or characteristics[i] == 0:
+            continue
+        value += 1000*(1-abs(point.characteristics[i]-characteristics[i]))
+        
+    return heuristic(start, midpoint, map) + heuristic(midpoint, end, map) - value
+
 def get_neighbors(point_id, map):
+    """
+    Obtiene los puntos adyacentes a un punto en el mapa
+
+    Args:
+        point_id (num): Id del punto a analizar
+        map (object): Datos del mapa, incluyendo información sobre puntos de interés y caminos.
+
+
+    Returns:
+        neighbors: Lista de vecinos del nodo especificado
+    """
+    
     neighbors = []
     for neighbor in map.paths[point_id]:
             neighbors.append(neighbor)
     return neighbors
 
-# Reconstruir el camino desde el inicio hasta el final
 def reconstruct_path(came_from, current, map):
+    """
+    Reconstruye el camino completo desde el punto inicial hasta el objetivo final teniendo en cuenta el array came_from.
+
+    Args:
+        came_from (list): Array con los diccionarios que guardan los datos de un punto objetivo a otro de los puntos y por qué punto fue descubierto en el algoritmo.
+        current (tuple): Coordenadas actuales del nodo actual.
+
+    Returns:
+        path (list): Puntos que forman el camino completo desde el punto inicial al final del mapa.
+
+    Note:
+        El array came_from contiene diccionarios con la información de los puntos descubiertos desde el punto inicio o desde un punto objetivo al próximo.
+        Como se tiene el punto final del mapa para conformar el camino se formará inicialmente el camino al revés, y una vez se llegue a un punto que no se
+        encuentre en el diccionario se supone que pertenezca al camino entre el par de objetivos anteriores.
+    """
+    
     came_from.reverse()
     
     path = [current]
