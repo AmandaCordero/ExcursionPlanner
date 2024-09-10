107789b8d6666105dd9f5899f4c553d380f9d758
anuno61044 <anuno61044@gmail.com>
2024-09-10 13:06:05 -0400

    work in AddPoint front view

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Point(models.Model):
+    id = models.AutoField(primary_key=True)
+    point_id = models.FloatField(unique=True)
+    location_x = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)],default=0.0)
+    location_y = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)],default=0.0)
+    characteristics = models.JSONField(default=list)
+    height = models.FloatField()
+
+    def __str__(self):
+        return f"Punto {self.point_id}"
