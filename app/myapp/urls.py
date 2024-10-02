755785f3ea3d5ce889d9dff1dca7b208414b9a2c
Amanda Cordero <amandacordero.1709@gmail.com>
2024-10-02 16:33:05 -0400

    fix routes

--- app/myapp/urls.py
+++ app/myapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from . import views
+from .views import edges
+from .views import points
+from .views import tourists
+from .views import llm
+from .views import statistics
+from .views import simulation
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,30 +13,30 @@ urlpatterns = [
     path('', views.pagina_inicio, name='pagina_inicio'),
     
     # Point urls
-    path('create_point/', views.create_point, name='create_point'),
-    path('create_point/<int:point_id>/', views.create_point, name='edit_point'),
-    path('delete_point/<int:point_id>/', views.delete_point, name='delete_point'),
-    path('points/', views.get_points, name='obtener_puntos'),
-    path('save_points/', views.save_points, name='save_points'),
+    path('create_point/', points.create_point, name='create_point'),
+    path('create_point/<int:point_id>/', points.create_point, name='edit_point'),
+    path('delete_point/<int:point_id>/', points.delete_point, name='delete_point'),
+    path('points/', points.get_points, name='obtener_puntos'),
+    path('save_points/', points.save_points, name='save_points'),
     
     # Edge urls
-    path('create_edge/', views.create_edge, name='create_edge'),
-    path('create_edge/<int:edgeId>/', views.create_edge, name='edit_edge'),
-    path('delete_edge/<int:edgeId>/', views.delete_edge, name='delete_edge'),
-    path('edges/', views.get_edges, name='get_edges'),
-    path('save_edges/', views.save_edges, name='save_edges'),
+    path('create_edge/', edges.create_edge, name='create_edge'),
+    path('create_edge/<int:edgeId>/', edges.create_edge, name='edit_edge'),
+    path('delete_edge/<int:edgeId>/', edges.delete_edge, name='delete_edge'),
+    path('edges/', edges.get_edges, name='get_edges'),
+    path('save_edges/', edges.save_edges, name='save_edges'),
     
     # Tourist urls
-    path('create_tourist/', views.create_tourist, name='create_tourist'),
-    path('create_tourist/<int:tourist_id>/', views.create_tourist, name='edit_tourist'),
-    path('delete_tourist/<int:tourist_id>/', views.delete_tourist, name='delete_tourist'),
-    path('tourists/', views.get_tourists, name='get_tourists'),
-    path('save_tourists/', views.save_tourists, name='save_tourists'),
+    path('create_tourist/', tourists.create_tourist, name='create_tourist'),
+    path('create_tourist/<int:tourist_id>/', tourists.create_tourist, name='edit_tourist'),
+    path('delete_tourist/<int:tourist_id>/', tourists.delete_tourist, name='delete_tourist'),
+    path('tourists/', tourists.get_tourists, name='get_tourists'),
+    path('save_tourists/', tourists.save_tourists, name='save_tourists'),
     
-    path('plan_route/', views.plan_route_info, name='plan_route'),
-    path('plan_route/get_info/', views.view_route_description, name='get_info'),
-    path('mostrar_markdown/', views.ver_encuesta, name='mostrar_markdown'),
-    path('run_simulate/', views.run_simulate, name='run_simulate'),
+    # path('plan_route/', simulation.plan_route_info, name='plan_route'),
+    path('plan_route/get_info/', llm.view_route_description, name='get_info'),
+    path('mostrar_markdown/', llm.ver_encuesta, name='mostrar_markdown'),
+    path('run_simulate/', simulation.run_simulate, name='run_simulate'),
 ]
 
 if settings.DEBUG:
